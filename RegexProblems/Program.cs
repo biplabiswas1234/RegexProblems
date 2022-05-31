f7269f8f98a756eca9307a4344a2631fc6ea2936
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-31 20:04:50 +0530

    [Biplab] Add: Validation of first name

---
+++ RegexProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RegexProblems
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            {
+                UserRegistration userRegistration = new UserRegistration();
+                Console.WriteLine("Enter the First Name");
+                string firstName = Console.ReadLine();
+                userRegistration.checkfirstname(firstName);
+            }
+        }
+    }
+}
+
---
+++ RegexProblems/UserRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    internal class UserRegistration
+    {
+        public string FirstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+
+
+        public void checkfirstname(string Firstname)
+        {
+            Regex Regex = new Regex(FirstName);
+            if (Regex.IsMatch(Firstname))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is Invalid");
+            }
+        }
+
+    }
+}
